b660321ebcb91167dc0b0a85e87bf184c358046a
Mandy Tsang <mandy.tsang007@gmail.com>
2022-03-05 11:35:29 -0800

    add styles.js

---
+++ src/styles.js
@@ -0,0 +1,41 @@
+import { makeStyles } from '@mui/styles';
+
+
+    // Create useStyles, a hook, w/ a function call to return an obj.
+const useStyles = makeStyles((theme) => ({
+        root: {
+            height: "100vh",
+            background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
+        },
+        imagelist: {
+            justify: "center",
+            width: "80vw",
+            height: "100vw",
+        },
+        buttons: {
+            marginRight: "20px",
+            marginTop: "40px",
+            textAlign: "center",
+            display: "flex",
+            justifyContent: "center",
+        },
+        image: {
+            width: "22vw",
+            height: "30vw",
+            marginTop: "5vw",
+        },
+        homeContainer: {
+            alignContent: "center",
+        },
+        clickableIcon: {
+            marginTop: "10vw",
+            paddingRight: "5vw",
+            transform: "scale(1.9)",
+            color: "black",
+            '&:hover': {
+                color: "red",
+            }
+        }
+    }));
+
+export default useStyles;
